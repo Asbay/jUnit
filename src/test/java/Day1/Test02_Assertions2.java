c0a3f13f5cbd91633766c17e2ac025bff3b1a178
AsBay <asenabayhan@outlook.de>
2023-02-01 21:52:49 +0100

    b

--- src/test/java/Day1/Test02_Assertions2.java
+++ src/test/java/Day1/Test02_Assertions2.java
@@ -2,6 +2,8 @@ package Day1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Test02_Assertions2 {
@@ -12,7 +14,14 @@ public class Test02_Assertions2 {
        // assertTrue(11.5 == Math.min(11.5,14.4)); //bu dogru degil mi anlamina gelir
         assertFalse(Math.min(12.3, 13.5)!= 12.3); // bu yanlis mi  geceemdi testi cünk+ü ici dogru
         //assertEquals(14, Math.min(14,17));
+    }
 
-
+    @Test
+    void testArrays(){
+        String str = "Test islemi cok kolay";
+        String[] anlikDizi = str.split(" ");
+        String [] beklenenDizi = {"Test", "islemi", "cok", "kolay"};
+        assertTrue(Arrays.equals(beklenenDizi,anlikDizi));
+        //assertArrayEquals(beklenenDizi,anlikDizi);
     }
 }
