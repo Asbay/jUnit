56d12fe4bb69a414d39886b4ca2d78aec58ee2fc
AsBay <asenabayhan@outlook.de>
2023-02-03 21:47:55 +0100

    as

---
+++ src/main/java/Day01/C04_Mock_Islemler.java
@@ -0,0 +1,23 @@
+package Day01;
+
+public class C04_Mock_Islemler {
+
+    public void ekleOgrenci(String ogrenciAdi){
+
+        System.out.println("Ogrenci eklendi");
+
+
+    }
+
+    public void silOgrenci(String ogrenciAdi){
+
+        System.out.println("Ogrenci kaydi silindi");
+    }
+
+    public void guncelleOgrenci(String ogrenciAdi){
+
+        System.out.println("Ogrenci bilgileri güncellendi");
+    }
+
+
+}
